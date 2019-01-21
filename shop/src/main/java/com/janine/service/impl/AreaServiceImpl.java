229c3614b7430a899e35d5d7639526daafc4dbb4
Janine <smilezyzyan@gmail.com>
2019-01-21 11:16:55 +0800

    spring管理service中的sqlsession

--- shop/src/main/java/com/janine/service/impl/AreaServiceImpl.java
+++ shop/src/main/java/com/janine/service/impl/AreaServiceImpl.java
@@ -5,10 +5,12 @@ import com.janine.pojo.Area;
 import com.janine.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class AreaServiceImpl implements AreaService {
     
     @Autowired
